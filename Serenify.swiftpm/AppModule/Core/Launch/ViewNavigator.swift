ec9cee24e3d92e869f57fb3f99de2e50c9349164
Elyan Gutierrez <elyann60@gmail.com>
2024-11-18 17:52:12 -0600

    Reworking navigation layout.

--- Serenify.swiftpm/AppModule/Core/Launch/ViewNavigator.swift
+++ Serenify.swiftpm/AppModule/Core/Launch/ViewNavigator.swift
@@ -13,7 +13,7 @@ struct ViewNavigator: View {
             Group {
                 BreathingView()
                     .tabItem {
-                        Label("Breathing", systemImage: "figure")
+                        Label("Breathing", systemImage: "wind")
                     }
                 
                 JournalView()
@@ -24,11 +24,6 @@ struct ViewNavigator: View {
                 MeditationView()
                     .tabItem {
                         Label("Meditation", systemImage: "figure.mind.and.body")
-                    }
-                
-                SettingsView()
-                    .tabItem {
-                        Label("Settings", systemImage: "gear")
                     }
             }
             .toolbarBackground(Color("darkerGray").opacity(0.97), for: .tabBar)
